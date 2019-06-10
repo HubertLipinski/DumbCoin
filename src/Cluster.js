90aff942982931188889f27afa7c8029fb76b986
HubertLipinski <hubertlipinskipl@gmail.com>
2019-06-10 14:17:38 +0200

    main.js refactoring

--- src/Cluster.js
+++ src/Cluster.js
@@ -10,17 +10,39 @@ const STATES = {
 };
 
 class Cluster {
-    constructor(id) {
-        this.id = id;
+    constructor(sw) {
+        this.id = sw.me;
         this.hostInfo = null;
-        this.pool = null;
+        this.peer = null;
         this.blockChain = new BlockChain();
         this.state = STATES.CREATED;
         this.timestamp = Date.now();
+
+        sw.on('connect', (peer) => {
+            this.peer = peer;
+            this.hostInfo = peer.address();
+            this.reciveData();
+            peer.send(JSON.stringify({test:true}))
+        });
+
+        sw.on('peer', function (peer, id) {
+            console.log('Im now connected to a new peer:', id)
+        });
+
+        sw.on('disconnect', (peer, id) => {
+            console.log("disconnected: ",id);
+        });
+
     }
 
     updateHostInfo(info) {
         this.hostInfo = info;
+    }
+
+    reciveData() {
+        this.peer.on('data', (data) => {
+            console.log(data)
+        });
     }
 
     info() {
