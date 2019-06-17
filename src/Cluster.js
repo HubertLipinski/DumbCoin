6617aaae7c117ef16727b33177c66be81f487e37
HubertLipinski <hubertlipinskipl@gmail.com>
2019-06-17 17:55:40 +0200

    changed concept of the network transfer

--- src/Cluster.js
+++ src/Cluster.js
@@ -1,9 +1,8 @@
 const BlockChain = require('./Models/Blockchain');
 
-const SHA256 = require('crypto-js/sha256');
-
-const net = require('net');
-
+const net = require('net'),
+      nssocket = require('nssocket'),
+      io = require('socket.io-client');
 
 /**
  * States in which a cluster may be.
@@ -20,6 +19,7 @@ const STATES = {
     INVALID: 'invalid cluster'
 };
 
+
 /**
  * This class is my representation of User in peer to peer network.
  * Every user starts with its own copy of blockchain wich must be synchronized beetwen other peers in network - using my own implementation of gossip protocol
@@ -30,108 +30,18 @@ const STATES = {
 class Cluster {
     /**
      * This is the Cluster constructor
-     * @param sw Swamp instance wich holds all information about network and it's peers
      */
-    constructor(sw) {
+    constructor() {
+        this.id = null;
         this.blockChain = new BlockChain();
         this.state = STATES.CREATED;
         this.timestamp = Date.now();
 
-        sw.on('connect', (peer) => {
-
-            this.peer = peer;
-            this.hostInfo = peer.address();
-            this.peerList = sw.peers;
-
-            //create chanell to recive data
-            const server = net.createServer((c) => {
-                // 'connection' listener
-                console.log('client connected');
-                c.on('end', () => {
-                    console.log('client disconnected');
-                });
-                c.write('hello\r\n');
-                c.pipe(c);
-            });
-
-            server.listen({
-                port: this.hostInfo.port,
-                host: this.hostInfo.address,
-                exclusive: true
-            });
-            this.reciveData();
-
-            // setInterval(()=>{
-            //     this.synchronizeData();
-            // },1000);
-
-        });
-
-        sw.on('peer', (peer, id) => {
-            console.log('Im now connected to a new peer:', id)
-        });
-
-        sw.on('disconnect', (peer, id) => {
-            console.log("disconnected: ",id);
-        });
-
+        //test only
+        const socket = io('http://127.0.0.1:3000');
 
     }
 
-    reciveData() {
-        this.peer.on('data', (data) => {
-            console.log(data)
-        });
-    }
-
-    /**
-     * This function iterates over all peers in list and takes their network information
-     * @returns {Array | null} Returns array of objects - only other hosts in network (current cluster data excluded)
-     */
-    getNetworkData() {
-        if (this.peerList) {
-            let connectedPeers = [];
-            for (let index = 0; index < this.peerList.length; index++) {
-                const peer = this.peerList[index];
-                connectedPeers.push({
-                        port: peer.remotePort,
-                        family: peer.remoteFamily,
-                        address: peer.remoteAddress
-                    }
-                );
-            }
-            return connectedPeers;
-        }
-        return null;
-    }
-
-    synchronizeData() {
-        let data = this.getNetworkData();
-        //todo get the random id between 0 and data.length
-        console.log("data 0;",data[0]);
-
-        const client = net.createConnection({
-                port: data[0].port,
-                host: data[0].address
-            },
-            () => {
-            // 'connect' listener
-            console.log('connected to server!');
-            client.write('world!\r\n');
-        });
-        client.on('data', (data) => {
-            console.log("received data: ",data.toString());
-            client.end();
-        });
-        client.on('end', () => {
-            console.log('disconnected from server');
-        });
-
-    }
-
-    info() {
-        console.log(this.sw.peers);
-    }
 
 }
 
